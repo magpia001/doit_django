d1ce613af77639bddcb69dd1bde91d6da8e8b66d
kyongha <magpia001@gmail.com>
2022-10-27 11:41:29 +0900

    포스트 리스트에 부트스트랩 적용, 네비게이션바 적용

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import index, single_post_page
+# from .views import index, single_post_page
+from .views import PostList, PostDetail
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('<int:pk>/', single_post_page, name='post_detail')
+    # path('', index, name='index'),  # FBV 방식의 call
+    path('', PostList.as_view(), name='post_list'),  # CBV 방식의 call
+
+    # path('<int:pk>/', single_post_page, name='post_detail')
+    path('<int:pk>/', PostDetail.as_view(), name='post_detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
+# blog list up
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
 
-    return render(request, 'blog/index.html', {'posts':posts, })
+#     return render(request, 'blog/index.html', {'posts':posts, })
+class PostList(ListView):
+    model = Post   # 클래스명
+    # template_name = 'blog/index.html'
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
 
-    return render(request, 'blog/single_post_page.html', {'post':post, })
+# 하나의 post 상세 페이지
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
+
+#     return render(request, 'blog/single_post_page.html', {'post':post, })
+class PostDetail(DetailView):
+    model = Post
 
